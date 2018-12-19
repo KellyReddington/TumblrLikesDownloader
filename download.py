cbda3b9e5e457f46ce07afc3633aaea1ff476943
Rodrigo Damazio Bovendorp <rodrigo@damazio.org>
2018-12-19 13:32:48 -0800

    Using timestamps instead of offsets to handle large lists of likes

--- download.py
+++ download.py
@@ -5,6 +5,7 @@ import pytumblr
 import os
 import urllib
 import re
+import time
 from tumblr_keys import *
 
 client = pytumblr.TumblrRestClient(
@@ -23,6 +24,9 @@ def media_download(mylikes, dirname):
                 urllib.urlretrieve(url, dirname + '/' + name[0])
             print('[' + str(i+1) + '/' + str(len(mylikes['liked_posts'])) + '] Photo ' + name[0])
         elif mylikes['liked_posts'][i]['type'] == 'video':
+            if not 'video_url' in mylikes['liked_posts'][i]:
+              print('[' + str(i+1) + '/' + str(len(mylikes['liked_posts'])) + '] External video')
+              continue
             url = mylikes['liked_posts'][i]['video_url']
             name = re.findall(r'tumblr_\w+.\w+', url)
             urllib.urlretrieve(url, dirname + '/' + name[0])
@@ -35,23 +39,24 @@ def main():
     dirname = info['user']['name']
     blogurl = info['user']['blogs'][0]['url']
     likescount = info['user']['likes']
-    statepath = os.path.join(dirname, 'last_offset')
-    lastoffset = 0
+    statepath = os.path.join(dirname, 'last_timestamp')
+    lasttimestamp = int(time.time())
     if not os.path.exists(dirname):
         os.makedirs(dirname)
     else:
         with open(statepath, 'r') as f:
-		lastoffset = int(f.read())
+          lasttimestamp = int(f.read())
 
-    mylikes = 1
-    while mylikes:
-        print 'Downloading at offset ', lastoffset
-	mylikes = client.likes(offset=lastoffset, limit=50)
+    while True:
+        print('Downloading before %d' % lasttimestamp)
+	mylikes = client.likes(before=lasttimestamp, limit=50)
 	media_download(mylikes, dirname)
-        lastoffset += 50
+	if not '_links' in mylikes:
+          break
+	lasttimestamp = int(mylikes['_links']['next']['query_params']['before'])
 	if mylikes:
-            with open(statepath, 'wb') as f:
-                f.write(str(lastoffset))
+          with open(statepath, 'wb') as f:
+            f.write(str(lasttimestamp))
 
 
 if __name__ == '__main__':
